d5d15dfd5a6920c18dbd442b1ae9c520df030acd
xchmel33 <xchmel33@users.noreply.github.com>
2021-06-23 23:50:07 +0200

    x

--- bytychtelnica/javascript/dropNav.js
+++ bytychtelnica/javascript/dropNav.js
@@ -1,9 +1,11 @@
 function showDropNav() {
-  document.getElementById('hidden').style.display = "inline-block";
+  document.getElementById('hiddenHover').style.display = "block";
+  document.getElementById('hidden').style.display = "block";
   document.getElementById('bNav').style.filter = "contrast(70%)";
 }
 
 function hideDropNav() {
+  document.getElementById('hiddenHover').style.display = "none";
   document.getElementById('hidden').style.display = "none";
   document.getElementById('bNav').style.filter = "none";
 }
