7441d605a37c01fac75f8a38d7bc2a298ed721d7
xchmel33 <xchmel33@users.noreply.github.com>
2021-06-23 19:02:42 +0200

    podlazia

---
+++ bytychtelnica/javascript/highlightB.js
@@ -0,0 +1,197 @@
+function highlightElement(x, y, z) {
+  switch (x) {
+    case 'P1':
+      document.getElementById('P1').style.opacity = '0.7';
+      document.getElementById('highlighterP1').style.opacity = '0.7';
+      break;
+    case 'P2':
+      document.getElementById('P2').style.opacity = '0.7';
+      document.getElementById('highlighterP2').style.opacity = '0.7';
+      break;
+    case 'P3':
+      document.getElementById('P3').style.opacity = '0.7';
+      document.getElementById('highlighterP3').style.opacity = '0.7';
+      break;
+    case 'P4':
+      document.getElementById('P4').style.opacity = '0.7';
+      document.getElementById('highlighterP4').style.opacity = '0.7';
+      break;
+    case 'P5':
+      document.getElementById('P5').style.opacity = '0.7';
+      document.getElementById('highlighterP5').style.opacity = '0.7';
+      break;
+    case 'P6':
+      document.getElementById('P6').style.opacity = '0.7';
+      document.getElementById('highlighterP6').style.opacity = '0.7';
+      break;
+    case 'P7':
+      document.getElementById('P7').style.opacity = '0.7';
+      document.getElementById('highlighterP7').style.opacity = '0.7';
+      break;
+    case 'P8':
+      document.getElementById('P8').style.opacity = '0.7';
+      document.getElementById('highlighterP8').style.opacity = '0.7';
+      break;
+    case 'P9':
+      document.getElementById('P9').style.opacity = '0.7';
+      document.getElementById('highlighterP9').style.opacity = '0.7';
+      break;
+  }
+  switch (y) {
+    case 'P10':
+      document.getElementById('highlighterP10').style.opacity = '0.7';
+      break;
+    case 'P11':
+      document.getElementById('highlighterP11').style.opacity = '0.7';
+      break;
+    case 'P12':
+      document.getElementById('highlighterP12').style.opacity = '0.7';
+      break;
+    case 'P13':
+      document.getElementById('highlighterP13').style.opacity = '0.7';
+      break;
+    case 'P14':
+      document.getElementById('highlighterP14').style.opacity = '0.7';
+      break;
+    case 'P15':
+      document.getElementById('highlighterP15').style.opacity = '0.7';
+      break;
+    case 'P16':
+      document.getElementById('highlighterP16').style.opacity = '0.7';
+      break;
+    case 'P17':
+      document.getElementById('highlighterP17').style.opacity = '0.7';
+      break;
+    case 'P18':
+      document.getElementById('highlighterP18').style.opacity = '0.7';
+      break;
+  }
+  switch (z) {
+    case 'P19':
+      document.getElementById('highlighterP19').style.opacity = '0.7';
+      break;
+    case 'P20':
+      document.getElementById('highlighterP20').style.opacity = '0.7';
+      break;
+    case 'P21':
+      document.getElementById('highlighterP21').style.opacity = '0.7';
+      break;
+    case 'P22':
+      document.getElementById('highlighterP22').style.opacity = '0.7';
+      break;
+    case 'P23':
+      document.getElementById('highlighterP23').style.opacity = '0.7';
+      break;
+    case 'P24':
+      document.getElementById('highlighterP24').style.opacity = '0.7';
+      break;
+    case 'P25':
+      document.getElementById('highlighterP25').style.opacity = '0.7';
+      break;
+    case 'P26':
+      document.getElementById('highlighterP26').style.opacity = '0.7';
+      break;
+    case 'P27':
+      document.getElementById('highlighterP27').style.opacity = '0.7';
+      break;
+  }
+}
+
+function unhighlightElement(x, y, z) {
+  switch (x) {
+    case 'P1':
+      document.getElementById('P1').style.opacity = '1';
+      document.getElementById('highlighterP1').style.opacity = '0';
+      break;
+    case 'P2':
+      document.getElementById('P2').style.opacity = '1';
+      document.getElementById('highlighterP2').style.opacity = '0';
+      break;
+    case 'P3':
+      document.getElementById('P3').style.opacity = '1';
+      document.getElementById('highlighterP3').style.opacity = '0';
+      break;
+    case 'P4':
+      document.getElementById('P4').style.opacity = '1';
+      document.getElementById('highlighterP4').style.opacity = '0';
+      break;
+    case 'P5':
+      document.getElementById('P5').style.opacity = '1';
+      document.getElementById('highlighterP5').style.opacity = '0';
+      break;
+    case 'P6':
+      document.getElementById('P6').style.opacity = '1';
+      document.getElementById('highlighterP6').style.opacity = '0';
+      break;
+    case 'P7':
+      document.getElementById('P7').style.opacity = '1';
+      document.getElementById('highlighterP7').style.opacity = '0';
+      break;
+    case 'P8':
+      document.getElementById('P8').style.opacity = '1';
+      document.getElementById('highlighterP8').style.opacity = '0';
+      break;
+    case 'P9':
+      document.getElementById('P9').style.opacity = '1';
+      document.getElementById('highlighterP9').style.opacity = '0';
+      break;
+  }
+  switch (y) {
+    case 'P10':
+      document.getElementById('highlighterP10').style.opacity = '0';
+      break;
+    case 'P11':
+      document.getElementById('highlighterP11').style.opacity = '0';
+      break;
+    case 'P12':
+      document.getElementById('highlighterP12').style.opacity = '0';
+      break;
+    case 'P13':
+      document.getElementById('highlighterP13').style.opacity = '0';
+      break;
+    case 'P14':
+      document.getElementById('highlighterP14').style.opacity = '0';
+      break;
+    case 'P15':
+      document.getElementById('highlighterP15').style.opacity = '0';
+      break;
+    case 'P16':
+      document.getElementById('highlighterP16').style.opacity = '0';
+      break;
+    case 'P17':
+      document.getElementById('highlighterP17').style.opacity = '0';
+      break;
+    case 'P18':
+      document.getElementById('highlighterP18').style.opacity = '0';
+      break;
+  }
+  switch (z) {
+  case 'P19':
+    document.getElementById('highlighterP19').style.opacity = '0';
+    break;
+  case 'P20':
+    document.getElementById('highlighterP20').style.opacity = '0';
+    break;
+  case 'P21':
+    document.getElementById('highlighterP21').style.opacity = '0';
+    break;
+  case 'P22':
+    document.getElementById('highlighterP22').style.opacity = '0';
+    break;
+  case 'P23':
+    document.getElementById('highlighterP23').style.opacity = '0';
+    break;
+  case 'P24':
+    document.getElementById('highlighterP24').style.opacity = '0';
+    break;
+  case 'P25':
+    document.getElementById('highlighterP25').style.opacity = '0';
+    break;
+  case 'P26':
+    document.getElementById('highlighterP26').style.opacity = '0';
+    break;
+  case 'P27':
+    document.getElementById('highlighterP27').style.opacity = '0';
+    break;
+}
+}
---
+++ bytychtelnica/javascript/highlightP.js
@@ -0,0 +1,185 @@
+function highlightElement(x) {
+  switch (x) {
+    case 'P1':
+      document.getElementById('P1').style.opacity = '0.7';
+      document.getElementById('highlighterP1').style.opacity = '0.7';
+      break;
+    case 'P2':
+      document.getElementById('P2').style.opacity = '0.7';
+      document.getElementById('highlighterP2').style.opacity = '0.7';
+      break;
+    case 'P3':
+      document.getElementById('P3').style.opacity = '0.7';
+      document.getElementById('highlighterP3').style.opacity = '0.7';
+      break;
+    case 'P4':
+      document.getElementById('P4').style.opacity = '0.7';
+      document.getElementById('highlighterP4').style.opacity = '0.7';
+      break;
+    case 'P5':
+      document.getElementById('P5').style.opacity = '0.7';
+      document.getElementById('highlighterP5').style.opacity = '0.7';
+      break;
+    case 'P6':
+      document.getElementById('P6').style.opacity = '0.7';
+      document.getElementById('highlighterP6').style.opacity = '0.7';
+      break;
+    case 'P7':
+      document.getElementById('P7').style.opacity = '0.7';
+      document.getElementById('highlighterP7').style.opacity = '0.7';
+      break;
+    case 'P8':
+      document.getElementById('P8').style.opacity = '0.7';
+      document.getElementById('highlighterP8').style.opacity = '0.7';
+      break;
+    case 'P9':
+      document.getElementById('P9').style.opacity = '0.7';
+      document.getElementById('highlighterP9').style.opacity = '0.7';
+      break;
+    case 'P10':
+      document.getElementById('P10').style.opacity = '0.7';
+      document.getElementById('highlighterP10').style.opacity = '0.7';
+      break;
+    case 'P11':
+      document.getElementById('P11').style.opacity = '0.7';
+      document.getElementById('highlighterP11').style.opacity = '0.7';
+      break;
+    case 'P12':
+      document.getElementById('P12').style.opacity = '0.7';
+      document.getElementById('highlighterP12').style.opacity = '0.7';
+      break;
+    case 'P13':
+      document.getElementById('P13').style.opacity = '0.7';
+      document.getElementById('highlighterP13').style.opacity = '0.7';
+      break;
+    case 'P14':
+      document.getElementById('P14').style.opacity = '0.7';
+      document.getElementById('highlighterP14').style.opacity = '0.7';
+      break;
+    case 'P15':
+      document.getElementById('P15').style.opacity = '0.7';
+      document.getElementById('highlighterP15').style.opacity = '0.7';
+      break;
+    case 'P16':
+      document.getElementById('P16').style.opacity = '0.7';
+      document.getElementById('highlighterP16').style.opacity = '0.7';
+      break;
+    case 'P17':
+      document.getElementById('P17').style.opacity = '0.7';
+      document.getElementById('highlighterP17').style.opacity = '0.7';
+      break;
+    case 'P18':
+      document.getElementById('P18').style.opacity = '0.7';
+      document.getElementById('highlighterP18').style.opacity = '0.7';
+      break;
+    case 'P19':
+      document.getElementById('P19').style.opacity = '0.7';
+      document.getElementById('highlighterP19').style.opacity = '0.7';
+      break;
+    case 'P20':
+      document.getElementById('P20').style.opacity = '0.7';
+      document.getElementById('highlighterP20').style.opacity = '0.7';
+      break;
+    case 'P21':
+      document.getElementById('P21').style.opacity = '0.7';
+      document.getElementById('highlighterP21').style.opacity = '0.7';
+      break;
+    case 'P22':
+      document.getElementById('P22').style.opacity = '0.7';
+      document.getElementById('highlighterP22').style.opacity = '0.7';
+      break;
+  }
+}
+
+function unhighlightElement(x) {
+  switch (x) {
+    case 'P1':
+      document.getElementById('P1').style.opacity = '1';
+      document.getElementById('highlighterP1').style.opacity = '0';
+      break;
+    case 'P2':
+      document.getElementById('P2').style.opacity = '1';
+      document.getElementById('highlighterP2').style.opacity = '0';
+      break;
+    case 'P3':
+      document.getElementById('P3').style.opacity = '1';
+      document.getElementById('highlighterP3').style.opacity = '0';
+      break;
+    case 'P4':
+      document.getElementById('P4').style.opacity = '1';
+      document.getElementById('highlighterP4').style.opacity = '0';
+      break;
+    case 'P5':
+      document.getElementById('P5').style.opacity = '1';
+      document.getElementById('highlighterP5').style.opacity = '0';
+      break;
+    case 'P6':
+      document.getElementById('P6').style.opacity = '1';
+      document.getElementById('highlighterP6').style.opacity = '0';
+      break;
+    case 'P7':
+      document.getElementById('P7').style.opacity = '1';
+      document.getElementById('highlighterP7').style.opacity = '0';
+      break;
+    case 'P8':
+      document.getElementById('P8').style.opacity = '1';
+      document.getElementById('highlighterP8').style.opacity = '0';
+      break;
+    case 'P9':
+      document.getElementById('P9').style.opacity = '1';
+      document.getElementById('highlighterP9').style.opacity = '0';
+      break;
+    case 'P10':
+      document.getElementById('P10').style.opacity = '1';
+      document.getElementById('highlighterP10').style.opacity = '0';
+      break;
+    case 'P11':
+      document.getElementById('P11').style.opacity = '1';
+      document.getElementById('highlighterP11').style.opacity = '0';
+      break;
+    case 'P12':
+      document.getElementById('P12').style.opacity = '1';
+      document.getElementById('highlighterP12').style.opacity = '0';
+      break;
+    case 'P13':
+      document.getElementById('P13').style.opacity = '1';
+      document.getElementById('highlighterP13').style.opacity = '0';
+      break;
+    case 'P14':
+      document.getElementById('P14').style.opacity = '1';
+      document.getElementById('highlighterP14').style.opacity = '0';
+      break;
+    case 'P15':
+      document.getElementById('P15').style.opacity = '1';
+      document.getElementById('highlighterP15').style.opacity = '0';
+      break;
+    case 'P16':
+      document.getElementById('P16').style.opacity = '1';
+      document.getElementById('highlighterP16').style.opacity = '0';
+      break;
+    case 'P17':
+      document.getElementById('P17').style.opacity = '1';
+      document.getElementById('highlighterP17').style.opacity = '0';
+      break;
+    case 'P18':
+      document.getElementById('P18').style.opacity = '1';
+      document.getElementById('highlighterP18').style.opacity = '0';
+      break;
+    case 'P19':
+      document.getElementById('P19').style.opacity = '1';
+      document.getElementById('highlighterP19').style.opacity = '0';
+      break;
+    case 'P20':
+      document.getElementById('P20').style.opacity = '1';
+      document.getElementById('highlighterP20').style.opacity = '0';
+      break;
+    case 'P21':
+      document.getElementById('P21').style.opacity = '1';
+      document.getElementById('highlighterP21').style.opacity = '0';
+      break;
+    case 'P22':
+      document.getElementById('P22').style.opacity = '1';
+      document.getElementById('highlighterP22').style.opacity = '0';
+      break;
+  }
+}
